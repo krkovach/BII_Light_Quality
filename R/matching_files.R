1ae019294d36708f696baa6c756c31c2933a6712
Jose Antonio Guzman Quesada <guzman@umn.edu>
2022-04-07 14:32:25 -0500

    implementingg matching of files function

--- R/matching_files.R
+++ R/matching_files.R
@@ -17,25 +17,24 @@ library(lubridate)
 #' @param threshold A interger describing the threshold in seconds to match with 
 #' the master file.
  
-file1 <- fread("J:/ligth_quality/Data processing/Pyranometer/FAB_pyranometer.txt")
-file2 <- fread("J:/ligth_quality/Data processing/BLK-C/FAB_BLK-C.txt")
-
+#file1 <- fread("E:/ligth_quality/Data processing/Pyranometer/FAB_pyranometer.txt")
+#file2 <- fread("E:/ligth_quality/Data processing/BLK-C/FAB_BLK-C.txt")
 
 #-------------------------------------------------------------------------------
 #' Function
 
-matching_files <- function(file1, file2, threshold = 300) {
+matching_files <- function(file1, file2, threshold = 9.99) {
   
   #Matching function
   date_time_1 <- ymd_hms(paste0(file1$date, " ", file1$time))
   date_time_2 <- ymd_hms(paste0(file2$date, " ", file2$time))
   
   #Anchor vector
-  return <- data.table(master = 1:length(date_time_1), 
-                       anchor = rep(NA, length(date_time_1)))
+  complete <- data.table(rows_1 = 1:length(date_time_1), 
+                         rows_2 = rep(NA, length(date_time_1)))
   
   #Loop over file1 rows
-  for(i in 1:length(anchor)) {
+  for(i in 1:length(date_time_1)) {
     
     #Estimate difference
     difference <- abs(date_time_1[i] - date_time_2)
@@ -46,14 +45,19 @@ matching_files <- function(file1, file2, threshold = 300) {
     #Value
     value <- as.numeric(difference[min_value], units = "secs")
     
-    
+    #Threshold logic
     if(value <= threshold) {
       
-      return$anchor[i] <- min_value
+      complete$rows_2[i] <- min_value
       
     }
   }
   
+  #Clean return
+  complete <- na.exclude(complete)
+  
+  #Return frame
+  return(complete)
   
 }
 
